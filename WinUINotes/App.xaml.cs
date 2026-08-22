ac7f30cc93fd9c72ef5a1c59f986956a3e8aecc0
Ekas <edvis.kulka@gmail.com>
2026-08-22 16:46:10 +0300

    Add project files

---
+++ WinUINotes/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using WinUINotes.ViewModels;
+
+namespace WinUINotes;
+
+public partial class App : Application
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public App()
+    {
+        Services = ConfigureServices();
+        this.InitializeComponent();
+    }
+
+    private static IServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+
+        // Services
+        services.AddSingleton<Services.IFileService>(x =>
+            ActivatorUtilities.CreateInstance<Services.WindowsFileService>(x,
+                            Windows.Storage.ApplicationData.Current.LocalFolder)
+        );
+
+        // ViewModels
+        services.AddTransient<AllNotesViewModel>();
+        services.AddTransient<NoteViewModel>();
+
+        return services.BuildServiceProvider();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        m_window = new MainWindow();
+        m_window.Activate();
+    }
+
+    public IServiceProvider Services { get; }
+
+    private Window? m_window;
+
+    public new static App Current => (App)Application.Current;
+}
---
+++ WinUINotes/Views/NotePage.xaml.cs
@@ -0,0 +1,42 @@
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using WinUINotes.Models;
+using WinUINotes.ViewModels;
+
+namespace WinUINotes.Views
+{
+    public sealed partial class NotePage : Page
+    {
+        private NoteViewModel? noteVm;
+
+        public NotePage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void RegisterForDeleteMessages()
+        {
+            WeakReferenceMessenger.Default.Register<NoteDeletedMessage>(this, (r, m) =>
+            {
+                if (Frame.CanGoBack)
+                {
+                    Frame.GoBack();
+                }
+            });
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            noteVm = App.Current.Services.GetService<NoteViewModel>();
+            RegisterForDeleteMessages();
+
+            if (e.Parameter is Note note && noteVm is not null)
+            {
+                noteVm.InitializeForExistingNote(note);
+            }
+        }
+    }
+}
