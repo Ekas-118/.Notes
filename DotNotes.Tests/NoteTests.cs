3d16239ebd4f0460097af917e1cbe8b9e26b00b6
Ekas <edvis.kulka@gmail.com>
2026-08-25 20:26:46 +0300

    Create new unit test project

---
+++ DotNotes.Tests/NoteTests.cs
@@ -0,0 +1,23 @@
+﻿using DotNotes.Bus.ViewModels;
+using DotNotes.Tests.Fakes;
+
+namespace DotNotes.Tests
+{
+    [TestClass]
+    public partial class NoteTests
+    {
+        [TestMethod]
+        public void TestCreateUnsavedNote()
+        {
+            var noteVm = new NoteViewModel(new FakeFileService());
+            Assert.IsNotNull(noteVm);
+            Assert.IsGreaterThan(DateTime.Now.AddHours(-1), noteVm.Date);
+            Assert.EndsWith(".txt", noteVm.Filename);
+            Assert.StartsWith("notes", noteVm.Filename);
+            noteVm.Text = "Sample Note";
+            Assert.AreEqual("Sample Note", noteVm.Text);
+            noteVm.SaveCommand.Execute(null);
+            Assert.AreEqual("Sample Note", noteVm.Text);
+        }
+    }
+}
